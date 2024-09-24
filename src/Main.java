7360eed7259b14e14c0a9c96ef15ac1cc70ffdce
aviv harari <shfanfan@yahoo.com>
2024-09-24 22:00:07 +0300

    sample code fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        Die d1 = new Die();
+        Die d2 = new Die();
+        System.out.println(d1.getNum());
+        System.out.println(d2.getNum());
+        System.out.println(d1.getNum()<d2.getNum());
+        d1.roll();
+        d2.roll();
+        System.out.print(d1.getNum());
+        System.out.print("-");
+        System.out.println(d2.getNum());
+        System.out.println(d1.getNum()+d2.getNum());
+        System.out.println(d1.getNum()+","+d2.getNum());
     }
 }
